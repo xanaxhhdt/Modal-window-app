6c87f40e029816fafefed496b7d2a82b4a1db214
Vlad <nicochan00@mail.u>
2022-06-29 23:14:19 +0300

    initial technical part

--- plugins/modal.js
+++ plugins/modal.js
@@ -1,6 +1,39 @@
 'use strict';
-
+function _createModal(options) {
+   const modal = document.createElement('div');
+   modal.classList.add('modal');
+   modal.insertAdjacentHTML('afterbegin', ` 
+      <div class="modal__overlay">
+         <div class="modal__window">
+            <div class="modal__header">
+               <span class="modal__title">Modal Title</span>
+               <span class="modal__close">&times</span>
+            </div>
+            <div class="modal__body">
+               <p>Lorem ipsum dolor sit.</p>
+               <p>Lorem ipsum dolor sit.</p>
+            </div>
+            <div class="modal__footer">
+               <button>ok</button>
+               <button>cancel</button>
+            </div>
+         </div>
+      </div>
+   `);
+   document.querySelector('.container').after(modal);
+   return modal;
+}
 
 $.modal = function (options) {
-   
+   const $modal = _createModal(options);
+
+   return {
+      open() {
+         $modal.classList.add('open');
+      },
+      close() {
+         $modal.classList.remove('open');
+      },
+      destroy() { },
+   };
 };
